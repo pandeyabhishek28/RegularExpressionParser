08355ac09f49bf917d7ae70c2d415d03df692131
Abhishek Pandey <pandeyabhishek28@outlook.com>
2019-12-31 16:49:06 +0530

    fixed build error and updated cli version

--- ClientApp/src/app/regular-expression/regular-expression.component.ts
+++ ClientApp/src/app/regular-expression/regular-expression.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { url } from "inspector";
 
 @Component({
   selector: "app-regular-expression",
